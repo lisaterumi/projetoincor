cb515f6df977c48c31b198597c2af3d51509fd98
lisaterumi <lisa.terumi@gmail.com>
2021-12-02 12:16:51 -0300

    Create connect.py

---
+++ connect.py
@@ -0,0 +1,30 @@
+import cx_Oracle
+
+connection = None
+
+username = 'XXXXXX'
+password = 'XXXXXX'
+dsn = '172.22.132.113/anonipac'
+port = 1521
+encoding = 'UTF-8'
+service_name= 'anonipac'
+table = 'PLN_HPC.PNL_TEXTO_ATENDIMENTO'
+
+try:
+    connection = cx_Oracle.connect(
+        username,
+        password,
+        dsn,
+        encoding=encoding)
+
+    # imprime la version de la base de datos
+    print(connection.version)
+	  print("select count(*) from PLN_HPC.PNL_TEXTO_ATENDIMENTO")
+
+except cx_Oracle.Error as error:
+    print(error)
+
+finally:
+    # release the connection
+    if connection:
+    connection.close()
